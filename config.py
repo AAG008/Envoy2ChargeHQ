211530ff85c9c7f1b7211db1d2088d74a5643e5e
AAG008 <129958259+AAG008@users.noreply.github.com>
2023-04-05 22:12:56 +1000

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+unlock = 'https://envoyIP/api/v1/production'
+source = 'https://envoyIP/ivp/livedata/status'
+endPoint = 'https://api.chargehq.net/api/public/push-solar-data'
+token = 'Yourgeneratetoken'
