9e7ad307a158a40ed0a49cafff46c70f96c12055
AAG008 <129958259+AAG008@users.noreply.github.com>
2023-04-17 14:19:41 +1000

    Update config.py
    Add comments

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-unlock = 'https://envoyIP/api/v1/production'
-source = 'https://envoyIP/production.json'
-endPoint = 'https://api.chargehq.net/api/public/push-solar-data'
-token = 'Yourgeneratetoken' # Run the token.py to get the value
+unlock = 'https://envoyIP/api/v1/production' # URL to use token to login to and gett session token for
+source = 'https://envoyIP/production.json' # URL that host production data
+endPoint = 'https://api.chargehq.net/api/public/push-solar-data' # Charge HQ's Public endpoint
+token = 'Yourgeneratetoken' # Run the token.py to get the value add it here
