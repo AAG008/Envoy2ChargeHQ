2c8aa947621e0acdb38343e993c15a40feb6fc6d
AAG008 <129958259+AAG008@users.noreply.github.com>
2023-04-17 14:02:34 +1000

    Update config.py
    updated URL's

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 unlock = 'https://envoyIP/api/v1/production'
-source = 'https://envoyIP/ivp/livedata/status'
+source = 'https://envoyIP/production.json'
 endPoint = 'https://api.chargehq.net/api/public/push-solar-data'
-token = 'Yourgeneratetoken'
+token = 'Yourgeneratetoken' # Run the token.py to get the value
